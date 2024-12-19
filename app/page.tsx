6871705ff60d7f61df0402a17552df973bb232bb
Harsh Kasat <harshkasat01@gmail.com>
2024-12-19 18:13:50 +0530

    adding post url

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
     setIsLoading(true);
   
     try {
-      const response = await fetch("http://127.0.0.1:8000/", {
+      const response = await fetch("https://readmeprautomate.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
